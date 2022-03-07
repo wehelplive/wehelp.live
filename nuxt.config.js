e88d74ab03825cf790461085ab13657cf64583c9
Aleksey Razbakov <aleksey@razbakov.com>
2022-03-07 20:15:33 +0100

    add viewport width

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,4 +15,10 @@ export default {
       },
     },
   },
+  head: {
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+    ],
+  },
 };
