66a45f622bc2e5c5977e9e12a638f6507ffc7cd2
Aleksey Razbakov <aleksey@razbakov.com>
2022-03-13 13:03:31 +0100

    add calendar

---
+++ pages/calendar.vue
@@ -0,0 +1,9 @@
+<template>
+  <iframe
+    class="w-full h-full min-h-screen"
+    src="https://calendar.google.com/calendar/embed?src=9ds20qcopbrpmu7nufu884su1o%40group.calendar.google.com&ctz=Europe%2FBerlin"
+    style="border: 0"
+    frameborder="0"
+    scrolling="no"
+  ></iframe>
+</template>
