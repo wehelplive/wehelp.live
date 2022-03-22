fe229ef01ac848cef6f179c569c155af6155aaa5
Aleksey Razbakov <aleksey@razbakov.com>
2022-03-22 23:11:55 +0100

    new request: en / ru

--- pages/ru/request.vue
+++ pages/ru/request.vue
@@ -1,7 +1,7 @@
 <template>
   <iframe
     class="airtable-embed h-full min-h-screen"
-    src="https://airtable.com/embed/shrM6YQFVOPXVJ1wP?backgroundColor=gray"
+    src="https://airtable.com/embed/shrhN2chHOVEgqBnw?backgroundColor=gray"
     frameborder="0"
     onmousewheel=""
     width="100%"
