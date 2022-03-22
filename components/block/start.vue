47ddbb35f6549f7c4c221a2f3f92885eed76b1a7
Aleksey Razbakov <aleksey@razbakov.com>
2022-03-22 23:12:41 +0100

    update tagline

--- components/block/start.vue
+++ components/block/start.vue
@@ -12,8 +12,8 @@
         <p
           class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
         >
-          We offer a step-by-step evacuation plan from anywhere in Ukraine to
-          anywhere in Europe.
+          We help to solve complex problems in the Ukrainian refugee crisis in
+          Europe
         </p>
         <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
           <div class="rounded-md shadow">
