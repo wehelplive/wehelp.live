80daec4836de1d02d23dceb2376213a9be9ae359
Aleksey Razbakov <aleksey@razbakov.com>
2022-03-25 22:01:28 +0100

    add guest fomr in Russian

---
+++ pages/ru/guest.vue
@@ -0,0 +1,11 @@
+<template>
+  <iframe
+    class="airtable-embed h-full min-h-screen"
+    src="https://airtable.com/embed/shrCqacL47YsCYdGt?backgroundColor=gray"
+    frameborder="0"
+    onmousewheel=""
+    width="100%"
+    height="533"
+    style="background: transparent; border: 1px solid #ccc"
+  ></iframe>
+</template>
