3dc6b42ea9cd24d3c5e6fb4f08667e64f830d110
harshbrahmxatriya <harshappy15@gmail.com>
2022-03-29 15:54:33 +0530

    removing console log

--- components/airtable-demo.js
+++ components/airtable-demo.js
@@ -7,7 +7,6 @@ var base = new Airtable({ apiKey: "keyQ54SEzABIZEm9F" }).base(
 const table = base('Tasks');    
 const getRecords = async () => {
     const records = await table.select().firstPage();
-    console.log(records);
     return records
 }
 
