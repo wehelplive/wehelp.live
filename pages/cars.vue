d40be419cb5f9c45475dc18730c130ae380839f2
Valentina-Olekhnovich <volekh@gmail.com>
2022-04-01 11:50:25 +0200

    Add id to key

--- pages/cars.vue
+++ pages/cars.vue
@@ -10,7 +10,7 @@
                 <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
                     <div class="space-y-12">
                     <ul card="list" class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
-                        <li v-for="record in cars.records" :key="record">
+                        <li v-for="record in cars.records" :key="record.id">
                         <div class="space-y-4">
                             <div class="aspect-w-3 aspect-h-2">
                             <img class="object-cover shadow-lg rounded-lg" v-bind:src='record.fields.Attachments[0].url' alt="">
