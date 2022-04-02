dd6fca03e32c33a2510d2e022d585f146521d8b2
harshbrahmxatriya <harshappy15@gmail.com>
2022-04-02 11:21:42 +0530

    list to display record from airtable v6

--- pages/cars.vue
+++ pages/cars.vue
@@ -1,44 +1,50 @@
 <template>
-  <div
-    v-for="car in list"
-    :key="car.id"
-    :style="{
-      'background-color': car.fields.color ? `${car.fields.color}` : 'orange',
-    }"
-    class="container"
-  >
-    <div>
-      {{ car.fields.color }}
-    </div>
-    <div>
-      {{ car.fields.Name }}
-    </div>
-    <div>
-      {{ car.fields.year }}
-    </div>
-  </div>
+  <Suspense>
+    <template #default>
+      <div
+        v-for="car in list"
+        :key="car.id"
+        class="container"
+        style="{`background-color: ${car.fields.color} %`}"
+      >
+        <div>
+          {{ car.fields.color }}
+        </div>
+        <div>
+          {{ car.fields.Name }}
+        </div>
+        <div>
+          {{ car.fields.year }}
+        </div>
+      </div>
+    </template>
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
 </template>
+
 <script>
 export default {
-  name: 'Airtable',
-  data() {
+  async setup() {
+    const data = await $fetch(
+      'https://api.airtable.com/v0/appv6M4U4ckXcHzhU/Cars?api_key=keyQ54SEzABIZEm9F'
+    )
+    console.log(data.records)
     return {
-      list: [],
+      list: data.records,
     }
-  },
-  async created() {
-    const { data } = await useFetch('/api/adb')
-    this.list = data._rawValue.data
   },
 }
 </script>
+
 <style scoped>
 .container {
   display: flex;
   justify-content: center;
   align-items: center;
 
-  color: white;
+  color: rgb(94, 6, 6);
 }
 div {
   margin: 20px;
