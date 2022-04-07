68b9aef3490fba438f16707abbdd6021c7956356
Valentina-Olekhnovich <volekh@gmail.com>
2022-04-07 14:28:46 +0200

    remove comments

--- pages/add.vue
+++ pages/add.vue
@@ -1,19 +1,3 @@
-<!--
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
   <form
     class="space-y-8 divide-y divide-gray-200"
