ae676e07383ce958e4cb1b79e936d79058e0fd1c
Valentina-Olekhnovich <volekh@gmail.com>
2022-04-13 18:20:01 +0200

    feat: change start design

--- components/block/start.vue
+++ components/block/start.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="flex justify-center items-center min-h-screen">
-    <main class="max-w-7xl px-4 sm:mt-24">
-      <div class="text-center">
-        <h1
-          class="text-4xl tracking-tight font-extrabold text-grey-800 sm:text-5xl md:text-6xl"
-        >
-          <span class="block xl:inline">🇺🇦 Evacuation to</span>
-          {{ ' ' }}
-          <span class="block text-royal-blue-600 xl:inline">EU</span>
-        </h1>
-        <p
-          class="mt-3 max-w-md mx-auto text-base text-grey-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
-        >
-          We help to solve complex problems in the Ukrainian refugee crisis in
-          EU
-        </p>
-        <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-          <div class="rounded-md shadow">
-            <NuxtLink
-              to="/request"
-              class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white-500 bg-royal-blue-600 hover:bg-royal-blue-700 md:py-4 md:text-lg md:px-10"
-            >
-              I need help
-            </NuxtLink>
-          </div>
-          <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-            <NuxtLink
-              to="/get-involved"
-              class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-royal-blue-600 bg-white-500 hover:bg-grey-50 md:py-4 md:text-lg md:px-10"
-            >
-              I can help
-            </NuxtLink>
+  <div class="bg-gradient-to-r from-blue-500">
+    <div
+      class="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-200 to-yellow-100"
+    >
+      <main class="max-w-7xl px-4 sm:mt-24">
+        <div class="text-center">
+          <h1
+            class="text-4xl mt- tracking-tight uppercase font-extra text-gray-900 sm:text-5xl md:text-6xl"
+          >
+            <span class="block xl:inline">Evacuation to EU</span>
+          </h1>
+          <p
+            class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+          >
+            We help to solve complex problems in the Ukrainian refugee crisis in
+            EU
+          </p>
+          <div class="mt-8 max-w-3xl mx-auto sm:flex sm:justify-center md:mt-8">
+            <div class="rounded-3xl shadow">
+              <NuxtLink
+                to="/request"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-3xl text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10"
+              >
+                I need help
+              </NuxtLink>
+            </div>
+            <div class="mt-3 sm:mt-0 sm:ml-3">
+              <NuxtLink
+                to="/get-involved"
+                class="w-full underline underline-offset-1 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-green-600 hover:text-green-800 md:py-4 md:text-lg md:px-10"
+              >
+                I can help
+              </NuxtLink>
+            </div>
           </div>
         </div>
-        <div class="py-8">↓ Scroll to learn more ↓</div>
-      </div>
-    </main>
+        <div class="bg-no-repeat bg-top bg-favicon h-64 w-full"></div>
+      </main>
+    </div>
   </div>
 </template>
