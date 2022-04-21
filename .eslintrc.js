4c6b240b8e141f898d0e9e6b2a2bfd8443eb8117
Aleksey Razbakov <aleksey@razbakov.com>
2022-04-21 22:19:34 +0200

    fix code style

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'no-console': 'off',
+    'vue/no-v-html': 'off',
+    'vue/comment-directive': 'off',
+  },
+  ignorePatterns: ['functions'],
+}
