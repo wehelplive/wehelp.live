b649337ffacbb45946d13b05a13ba7627320da01
amanuela97 <babyzewdie@gmail.com>
2022-04-27 15:32:13 +0300

    fix: changed positioning and font

--- components/block/values.vue
+++ components/block/values.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="flex flex-col sm:items-center gap-8  min-h-screen ">
-      <h2 class="mx-7 font-nunito  mt-[5%]  style-basic text-headline-6 font-semibold">Our core values</h2>
-       <h2 class="mx-7 font-openSans  mt-[5%]  style-basic text-headline-6 font-semibold">Our core values</h2>
+  <div class="flex flex-col min-h-screen ">
+      <h2 class="ml-[25px] mr-[198px] mb-[45px]  w-[152px] h-[21px] font-nunito text-headline-6 font-semibold text-[#2b2b2b]">Our core values</h2>
 
-      <p class="bg-white mx-7  text-dark-grey text-subtitle-3"> 
-        We aim to be a <strong class="text-green">trustful</strong> source that give
-        people <strong class="text-orange">hope</strong> and help them through <strong class="text-blue">effective</strong> 
+      <p class="ml-[28px] mr-[16px] mb-[30px] w-[331px] h-[66px] font-nunito text-subtitle font-normal text-[#464646]"> 
+        We aim to be a <span class="font-bold text-green-500">trustful</span> source that give
+        people <span class="font-bold text-orange-500">hope</span> and help them through <span class="font-bold text-blue-500">effective</span> 
         actions
       </p>
 
-      <img src="/values.svg" class=" self-center max-w-[100%] h-auto object-cover" />
+      <img src="/values.svg" class="self-center mb-[80px] h-auto object-cover" />
   </div>
 </template>
