142bf79c5c4f6ce236d6054d2fcc5def5c0e59de
amanuela97 <babyzewdie@gmail.com>
2022-04-27 15:39:40 +0300

    fix: linting error in values

--- components/block/values.vue
+++ components/block/values.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="flex flex-col min-h-screen ">
-      <h2 class="ml-[25px] mr-[198px] mb-[45px]  w-[152px] h-[21px] font-nunito text-headline-6 font-semibold text-[#2b2b2b]">Our core values</h2>
+    <h2
+      class="ml-[25px] mr-[198px] mb-[45px]  w-[152px] h-[21px] font-nunito text-headline-6 font-semibold text-[#2b2b2b]"
+    >
+      Our core values
+    </h2>
 
-      <p class="ml-[28px] mr-[16px] mb-[30px] w-[331px] h-[66px] font-nunito text-subtitle font-normal text-[#464646]"> 
-        We aim to be a <span class="font-bold text-green-500">trustful</span> source that give
-        people <span class="font-bold text-orange-500">hope</span> and help them through <span class="font-bold text-blue-500">effective</span> 
-        actions
-      </p>
+    <p
+      class="ml-[28px] mr-[16px] mb-[30px] w-[331px] h-[66px] font-nunito text-subtitle font-normal text-[#464646]"
+    >
+      We aim to be a
+      <span class="font-bold text-green-500">trustful</span> source that give
+      people <span class="font-bold text-orange-500">hope</span> and help them
+      through <span class="font-bold text-blue-500">effective</span>
+      actions
+    </p>
 
-      <img src="/values.svg" class="self-center mb-[80px] h-auto object-cover" />
+    <img src="/values.svg" class="self-center mb-[80px] h-auto object-cover" />
   </div>
 </template>
