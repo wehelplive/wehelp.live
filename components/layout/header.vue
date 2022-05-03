34db59f8d0be455a12aadd140588f913b603f74a
harshbrahmxatriya <harshappy15@gmail.com>
2022-05-03 13:46:39 +0530

    Menu Icon and x icon positioning

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,18 +1,16 @@
 <template>
   <Popover class="relative bg-white">
     <div class="max-w-8xl z-50 shadow-md sm:px-6">
-      <div
-        class="flex justify-between items-center md:justify-start md:space-x-10"
-      >
+      <div class="flex justify-between items-center md:justify-start">
         <div class="flex justify-start mt-2 ml-2 lg:w-0 lg:flex-1">
           <NuxtLink to="/">
             <BrandLogo class="w-24 h-24" />
           </NuxtLink>
         </div>
         <div class="font-medium mt-5 text-gray-800">Hotline 24/7</div>
-        <div class="my-2 md:hidden">
+        <div class="mr-4 md:hidden">
           <PopoverButton
-            class="bg-white rounded-md mt-2 p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            class="bg-white rounded-md mt-1 p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
           >
             <span class="sr-only">Open menu</span>
             <MenuIcon class="h-6 w-6" aria-hidden="true" />
