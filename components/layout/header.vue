fa76448fa42d6bee59b498f3f556ea3e777a0d71
harshbrahmxatriya <harshappy15@gmail.com>
2022-05-05 16:03:30 +0530

    linting fix

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -85,8 +85,8 @@
               <div class="h-5"></div>
               <div class="m-6">
                 <select
-                  class="form-select w-18 h-12 rounded-lg block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   id="language"
+                  class="form-select w-18 h-12 rounded-lg block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                 >
                   <option>EN</option>
                   <option>DE</option>
