26104522556dfa7f4e03575c1f16b3d7dbfc07a2
Valentina-Olekhnovich <volekh@gmail.com>
2022-05-16 13:03:27 +0200

    change styles

--- components/block/team.vue
+++ components/block/team.vue
@@ -1,33 +1,29 @@
 <template>
-  <swiper
-    :modules="modules"
-    :slides-per-view="1"
-    :space-between="50"
-    navigation
-    :pagination="{ clickable: true }"
-    :scrollbar="{ draggable: true }"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
-    <swiper-slide class="h-300 line-height: 5rem bg-blue-500 	text-align: center"
-      >Slide 1</swiper-slide
+  <div>
+    <h1 class="text-center text-xl font-bold">What others say about us</h1>
+    <swiper
+      :modules="modules"
+      :slides-per-view="1"
+      :space-between="50"
+      :loop="true"
+      :pagination="pagination"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
     >
-    <swiper-slide class="h-300 line-height: 5rem bg-blue-500 text-align: center"
-      >Slide 2</swiper-slide
-    >
-    <swiper-slide class="h-300 line-height: 5rem bg-blue-500 text-align: center"
-      >Slide 3</swiper-slide
-    >
-  </swiper>
+      <!-- :pagination="{ clickable: true }" -->
+      <swiper-slide class="h-300 "><BlockVolunteer /> </swiper-slide>
+      <swiper-slide class="h-300 "><BlockVolunteer /></swiper-slide>
+      <swiper-slide class="h-300 "><BlockVolunteer /></swiper-slide>
+    </swiper>
+  </div>
 </template>
 <script>
-import { Navigation, Pagination } from 'swiper'
+import { Autoplay, Navigation, Pagination } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
 
 export default {
   components: {
@@ -44,17 +40,39 @@ export default {
     return {
       onSwiper,
       onSlideChange,
-      modules: [Navigation, Pagination],
+      pagination: {
+        clickable: true,
+        renderBullet(index, className) {
+          return '<span class="' + className + '">' + '</span>'
+        },
+      },
+      autoplay: {
+        delay: 3000,
+      },
+      modules: [Pagination, Autoplay],
     }
   },
 }
 </script>
 <style scoped>
-.swiper-slide {
-  height: 300px;
-  line-height: 300px;
-  font-size: 30px;
-  text-align: center;
-  background-color: pink;
+.swiper-pagination-bullet {
+  /** Stylization of bookmarks that are not selected */
+  width: var(
+    --swiper-pagination-bullet-width,
+    var(--swiper-pagination-bullet-size, 16px)
+  );
+  height: var(
+    --swiper-pagination-bullet-height,
+    var(--swiper-pagination-bullet-size, 16px)
+  );
+  display: inline-block;
+  border-radius: 50%;
+  background: var(--swiper-pagination-bullet-inactive-color, red);
+  opacity: var(--swiper-pagination-bullet-inactive-opacity, 0.2);
+}
+.swiper-pagination-bullet-active {
+  /** Marker styling of selected image in carousel */
+  opacity: var(--swiper-pagination-bullet-opacity, 1);
+  background: var(--swiper-pagination-color, var(--swiper-theme-color));
 }
 </style>
--- components/block/volunteer.vue
+++ components/block/volunteer.vue
@@ -1,37 +1,52 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <section class="bg-white-500">
-    <div class="max-w-7xl mx-auto md:px-6 lg:px-8">
-      <div class="py-12 px-4 sm:px-6 md:flex ">
-        <blockquote class="mt-6 ">
-          <header class="mt-8">
-            <div class="flex items-start">
-              <div class="flex-shrink-0 grayscale inline-flex rounded-full">
-                <img
-                  class="h-12 w-12 rounded-full"
-                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </div>
+  <section class="bg-white-500 m-10">
+    <div class="border-grey-200 rounded-md drop-shadow-md">
+      <div class="bg-white-500">
+        <div class="max-w-7xl   m-10  md:px-6 lg:px-8">
+          <div class="py-4 px-4 sm:px-6 md:flex ">
+            <blockquote class="m-4">
+              <header>
+                <div class="flex items-start">
+                  <div class="flex-shrink-0 grayscale inline-flex rounded-full">
+                    <img
+                      class="h-12 w-12 rounded-full"
+                      src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </div>
 
-              <div class="ml-4">
-                <div class="text-xl text-black-500">
-                  Judith Black
+                  <div class="ml-20">
+                    <div class="text-xl text-black-500">
+                      Judith Black
+                    </div>
+                    <div class="text-s text-black-500">
+                      Customer
+                    </div>
+                  </div>
                 </div>
+                <div class="flex justify-between mt-10">
+                  <div class="text-m text-black-500">
+                    Stars
+                  </div>
+
+                  <div class=" flex ml-auto text-m text-zink-700">
+                    25.07.2022
+                  </div>
+                </div>
+              </header>
+              <div
+                class="relative text-s pt-10 text text-zink-700 md:flex-grow"
+              >
+                <p class="relative">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                  expedita voluptas culpa sapiente alias molestiae. Numquam
+                  corrupti in laborum sed rerum et corporis.
+                </p>
               </div>
-              <div class="flex ml-auto">
-                <div class="mr-4 mt-1 text-l text-zink-700">25.07.2022</div>
-              </div>
-            </div>
-          </header>
-          <div class="relative text-l pt-10 text-zink-700 md:flex-grow">
-            <p class="relative">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-              expedita voluptas culpa sapiente alias molestiae. Numquam corrupti
-              in laborum sed rerum et corporis.
-            </p>
+            </blockquote>
           </div>
-        </blockquote>
+        </div>
       </div>
     </div>
   </section>
