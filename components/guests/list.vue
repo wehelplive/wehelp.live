e1af19c962c92b5e0dc4652c15dc894f3e5f456c
Valentina-Olekhnovich <volekh@gmail.com>
2022-06-06 11:47:48 +0200

    add status

--- components/guests/list.vue
+++ components/guests/list.vue
@@ -2,10 +2,12 @@
   <div class="grid grid-cols-auto-fit gap-6 p-1">
     <ul v-for="guest in guests" :key="guest.id" role="list">
       <GuestsItem
+        v-if="guest.fields.Status === 'Validated' || 'Published'"
         :field-id="guest.id"
         :name="guest.fields.Name"
         :story="guest.fields.Story"
         :photo="guest.fields.Photo"
+        :status="guest.fields.Status"
       />
     </ul>
   </div>
