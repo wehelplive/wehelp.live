8ab2d632c592f4802905943e76717ff9348fe262
amanuela97 <babyzewdie@gmail.com>
2022-06-15 13:14:12 +0300

    fix: hover & opacity of partners section

--- components/block/partners.vue
+++ components/block/partners.vue
@@ -5,7 +5,7 @@
     <div
       v-for="partner in partners"
       :key="partner.name"
-      class="flex align-middle gap-8 h-8 sm: sm:h-2/5 px-5 sm:px-0 grayscale"
+      class="flex align-middle gap-8 h-8 sm: sm:h-2/5 px-5 sm:px-0 grayscale opacity-50 hover:grayscale-0 hover:opacity-100"
     >
       <img :src="partner.imageUrl" :alt="partner.name" />
     </div>
