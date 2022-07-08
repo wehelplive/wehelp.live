8cecb901f41a43f49133c5b1eb297db24451e243
Amanuel Ayezabu <44023180+amanuela97@users.noreply.github.com>
2022-07-08 19:54:47 +0300

    fix: persist locale (#79)

--- components/locale/switcher.vue
+++ components/locale/switcher.vue
@@ -4,6 +4,7 @@
       <select
         v-model="$i18n.locale"
         class="form-select uppercase w-13 h-12 rounded-lg block w-full px-3 py-1.5 text-base font-normal text-slate-500 bg-white-500 bg-clip-padding bg-no-repeat border border-solid border-slate-200 transition ease-in-out m-0 focus:text-slate-500 focus:bg-white-500 focus:border-royal-blue-600 focus:outline-none"
+        @change="storeLocale"
       >
         <option
           v-for="(locale, i) in $i18n.availableLocales"
@@ -16,3 +17,15 @@
     </div>
   </div>
 </template>
+<script>
+export default {
+  setup() {
+    function storeLocale(e) {
+      localStorage.setItem('currentLocale', e.target.value)
+    }
+    return {
+      storeLocale,
+    }
+  },
+}
+</script>
